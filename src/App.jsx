fbd65946698484bb386503db52afd9d09b3d5ae9
Shardul <shardulkulk297@gmail.com>
2025-03-07 00:09:04 +0530

    Learnt about saving data in collection in firebase

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import {collection, addDoc, getDocs} from 'firebase/firestore'
 function App() {
 
   const [data, setData] = useState({
+    name: '',
+    phone: '',
     email: '',
     password: ''
   })
   const auth = getAuth(app);
+  const dbInstance = collection(database, 'users');
 
   const handleInput = (e)=>{
 
@@ -27,6 +30,15 @@ function App() {
     createUserWithEmailAndPassword(auth, data.email, data.password)
     .then((response)=>{
       console.log(response.user);
+
+      addDoc(dbInstance, {
+        name: data.name,
+        phone: data.phone,
+        email: data.email,
+      })
+
+
+      alert('User Created Successfully');
     })
     .catch((error)=>{
       console.log(error);
@@ -38,6 +50,7 @@ function App() {
     signInWithEmailAndPassword(auth, data.email, data.password)
     .then((response)=>{
       console.log(response.user);
+      alert('User Logged In Successfully');
     })
     .catch((error)=>{
       console.log(error);
@@ -46,15 +59,27 @@ function App() {
 
   }
  
+  const addData = ()=>{
+    addDoc(dbInstance, data)
+    .then((response)=>{
+      console.log(response);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
 
   return (
     <>
       <div>
+        <input name='name' onChange={e=>{handleInput(e)}} className='' placeholder='name' type="text" /> <br />
+        <input name='phone' onChange={e=>{handleInput(e)}} className='' placeholder='phone' type="text" /> <br />
         <input name='email'  onChange={e=>{handleInput(e)}} className='' placeholder='email' type="text" /> <br />
         <input  onChange={e=>{handleInput(e)}} name='password' placeholder='password' type="password" /><br />
         <button onClick={handleSubmit}>Sign Up</button>
         <br />
         <button onClick={handleLogIn} type="button" class="btn btn-primary">SignIn</button>
+        <button onClick={addData}>Add Data</button>
         
       </div>
     </>
