a8ffad0656a26668c5d7f515ebfeb1abd3268816
Shardul <shardulkulk297@gmail.com>
2025-03-06 23:35:51 +0530

    Learnt about Sign In

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import {app, database} from './firebaseconfig'
 import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth'
+import {collection, addDoc, getDocs} from 'firebase/firestore'
 
 
 function App() {
@@ -32,6 +33,18 @@ function App() {
       alert(error.message);
     })
   }
+
+  const handleLogIn = ()=>{
+    signInWithEmailAndPassword(auth, data.email, data.password)
+    .then((response)=>{
+      console.log(response.user);
+    })
+    .catch((error)=>{
+      console.log(error);
+      alert(error.message);
+    })
+
+  }
  
 
   return (
@@ -40,6 +53,9 @@ function App() {
         <input name='email'  onChange={e=>{handleInput(e)}} className='' placeholder='email' type="text" /> <br />
         <input  onChange={e=>{handleInput(e)}} name='password' placeholder='password' type="password" /><br />
         <button onClick={handleSubmit}>Sign Up</button>
+        <br />
+        <button onClick={handleLogIn} type="button" class="btn btn-primary">SignIn</button>
+        
       </div>
     </>
   )
